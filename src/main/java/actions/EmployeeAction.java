87e138c468f8987c2ef88ac135d3fc8535e9ac95
remika.inagaki <r_inagaki@allexceed.co.jp>
2022-06-03 11:44:08 +0900

    従業員destroyアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -217,7 +217,24 @@ public class EmployeeAction extends ActionBase {
 
     }
 
+    /**
+     * 論理削除を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void destroy() throws ServletException, IOException {
+    	//CSRF対策 token のチェック
+    	if (checkToken()) {
+    		//idを条件に従業員データを論理削除
+    		service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
 
+    		//セッションに削除官僚のフラッシュメッセージを設定
+    		putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+    		//一覧画面にリダイレクト
+    		redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+    	}
+    }
 
 
 
